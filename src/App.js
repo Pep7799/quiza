7661dcaa8d83720e4b4fed2e3ed82dadcc73b99a
Folami Oluwapelumi <folamioluwapelumi1@gmail.com>
2022-07-13 13:30:56 -0700

    setting up

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./component/Navbar";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+      <Navbar />
+      <div className="text-center p-4 mt-24 mb-16">
+        <h1 className="text-2xl font-bold mb-8 ">Welcome to Science Quiz</h1>
+        <p className="text-xl mt-24 leading-10">
+          Take our quick quiz, to help you grade <br></br> your knowledge about
+          general science.<br></br>Our quiz comprises of various questions
+          <br></br> from Easy, Medium, Hard. You get instant corrections.
+          <br></br> These Sets of questions would help build your understanding.
+          <br></br>
+          You can begin the quiz.
+        </p>
+      </div>
+      <div
+        className=" mx-auto 
+      transition duration-150 delay-100 ease-in-out hover:scale-110 text-center w-24 h-8 rounded-lg shadow-lg font-bold bg-green-600 text-white"
+      >
+        <Link to="/quiz">Start quiz</Link>
+      </div>
+      <div className="bg-indigo-800 mt-52 w-full text-center text-white font-bold ">
+        Copyright © Created by Remzeey and Pearl
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/questions.js
@@ -0,0 +1,112 @@
+export const questions = [
+  //new questions -changed positions of some(9/7/22)
+{
+text: "Which is the closest planet to the sun?",
+options: [
+{ id: 0, text: "Mercury", isCorrect: true },
+{ id: 1, text: "Earth", isCorrect: false },
+{ id: 2, text: "Saturn", isCorrect: false },
+{ id: 3, text: "Venus", isCorrect: false },
+],
+difficulty: "Easy"
+},
+
+{
+text: "What is the boiling point of water?",      
+options: [
+{ id: 0, text: "25°C", isCorrect: false },
+{ id: 1, text: "50°C", isCorrect: false },
+{ id: 2, text: "75°C", isCorrect: false},
+{ id: 3, text: "100°C", isCorrect: true},
+],
+difficulty: "Easy"
+},
+
+
+{
+text: "Who in 1786 invented the gold leaf electro scope?",
+options: [
+{ id: 0, text: "Thomas Edison", isCorrect: false },
+{ id: 1, text: "Michael faraday", isCorrect: false },
+{ id: 2, text: "Isaac Newton", isCorrect: false },
+{ id: 3, text: "Abraham Bennet", isCorrect: true },
+],
+difficulty: "Medium"
+},
+{
+text: "Who was the first person to design models of flying machine?",
+options: [
+{ id: 0, text: "Leonardo da Vinci", isCorrect: true },
+{ id: 1, text: "Nikola Tesla", isCorrect: false },
+{ id: 2, text: "John Newman", isCorrect: false },
+{ id: 3, text: "Andrew Scott", isCorrect: false },
+],
+difficulty: "Medium"
+},
+{
+text: "How many teeth does an adult human have?",
+options: [
+{ id: 0, text: "32", isCorrect: true },
+{ id: 1, text: "36", isCorrect: false },
+{ id: 2, text: "30", isCorrect: false },
+{ id: 3, text: "34", isCorrect: false },
+],
+difficulty: "Easy"
+},
+{
+text: "What is the biggest planet in our solar system?",
+options: [
+{ id: 0, text: "Mars", isCorrect: false },
+{ id: 1, text: "Jupiter", isCorrect: true },
+{ id: 2, text: "Saturn", isCorrect: false },
+{ id: 3, text: "Mercury", isCorrect: false },
+],
+difficulty: "Easy"
+},
+{
+text: "What is the hardest natural substance on Earth?",
+options: [
+{ id: 0, text: "krypton", isCorrect: false },
+{ id: 1, text: "Diamond", isCorrect: true },
+{ id: 2, text: "Uranium", isCorrect: false },
+{ id: 3, text: "Brass", isCorrect: false },
+],
+difficulty: "Easy"
+},
+{
+text: "The standard unit of measurement for energy is ____ ?",
+options: [
+{ id: 0, text: "Newton", isCorrect: false },
+{ id: 1, text: "Ampere", isCorrect: false },
+{ id: 2, text: "Joule", isCorrect: true },
+{ id: 3, text: "Watt", isCorrect: false },
+],
+difficulty: "Easy"
+},
+
+{
+text: "What type of energy eventually depletes?",
+options: [
+{ id: 0, text: "Kinetic", isCorrect: false },
+{ id: 1, text: "Renewable", isCorrect: false },
+{ id: 2, text: "Gravitational", isCorrect: false },
+{ id: 3, text: "Non-renewable", isCorrect: true },
+],
+difficulty: "Easy"
+},
+
+{
+text: "Which is the lightest element in the periodic table?",      
+options: [
+{ id: 0, text: "Helium", isCorrect: false },
+{ id: 1, text: "Nitrogen", isCorrect: false },
+{ id: 2, text: "Hydrogen", isCorrect: true},
+{ id: 3, text: "Carbon", isCorrect: false },
+],
+difficulty: "Easy"
+},
+
+
+
+
+];
